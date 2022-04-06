3a7195d45ee18c8916a31a29096e2f32f62ef261
Hiroya Takemura <hirotakemura210@gmail.com>
2022-04-06 11:42:26 +0900

    added some pictures

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,11 @@ import { Title } from "./components/Title";
 const App = () => {
   return (
     <>
-    <div>
-      <div style={outerStyle}>
-        <Ground />
-      </div>
-    </div>
+      <Ground />
     </>
   );
 }
 
-//style
-
-const outerStyle = {
-  width: "100vh",
-  height: "100vw",
-  transform: "rotate(-90deg) translateX(-100vh)",
-  transformOrigin: "top left",
-  overflowY: "scroll",
-  overflowX: "hidden",
-  position: "absolute",
-}
 
 
 export default App;
--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
-import grass from '../images/grass.png'
+import { useParallax } from 'react-scroll-parallax';
+import layer_1 from "../images/layer_1.png";
 
 export const Ground = () => {
-  return (
-    <div style={wrapperStyle}>
-      <img style={grassStyle} src={grass} alt="grass.png" />
-    </div>
-  )
+  return <img src={layer_1} alt="" />
 }
 
-const grassStyle = {
-  height: '20vh',
-  filter: 'grayScale(50%)',
-  width: '400%',
-  position: 'absolute',
-  bottom: '0',
-}
-
-const wrapperStyle = {
-  transform: "rotate(90deg) translateY(-100vh)",
-  transformOrigin: "top left",
-  height: '100vh',
-  width: '100vw',
-  position: "relative"
-}
