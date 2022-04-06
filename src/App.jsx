d69059cc84f4140a016d1ccef528b0bf38787b13
Hiroya Takemura <hirotakemura210@gmail.com>
2022-04-06 15:22:16 +0900

    arranged

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { ParallaxBg } from "./components/ParallaxBg";
+import { ParallaxProvider } from 'react-scroll-parallax';
 import { Title } from "./components/Title";
 
 
 const App = () => {
   return (
-    <>
+    <ParallaxProvider scrollAxis="horizontal">
       <ParallaxBg />
-    </>
+    </ParallaxProvider>
   );
 }
 
--- src/components/ParallaxBg.jsx
+++ src/components/ParallaxBg.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { useParallax } from 'react-scroll-parallax';
 import layer_1 from "../images/layer_1.png";
 import layer_2 from "../images/layer_2.png";
@@ -9,14 +9,93 @@ import layer_6 from "../images/layer_6.png";
 import styles from './ParallaxBg.module.css';
 
 export const ParallaxBg = () => {
+
+  const target = useRef(null);
+
+  const layerFour = useParallax({
+    speed: 12,
+    targetElement: target.current,
+  });
+
+  const layerThree = useParallax({
+    speed: 25,
+    targetElement: target.current,
+  });
+
+  const layerTwo = useParallax({
+    speed: 50,
+    targetElement: target.current,
+  });
+  const layerOne = useParallax({
+    speed: 100,
+    targetElement: target.current,
+  });
+  const layerFourExtend = 12 * 5 * -1;
+  const layerThreeExtend = 25 * 5 * -1;
+  const layerTwoExtend = 50 * 5 * -1;
+  const layerOneExtend = 200 * 5 * -1;
+ 
   return (
-    <div>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_6})`}}/>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_5})`}}/>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_4})`}}/>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_3})`}}/>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_2})`}}/>
-      <div className={styles.layer} style={{backgroundImage: `url(${layer_1})`}}/>
+    <div className={styles.root} ref={target}>
+      {/* LAYER 6 (BG) */}
+      <div 
+        className={styles.layer} 
+        style={{
+          backgroundImage: `url(${layer_6})`, 
+          left: 0,
+          right: 0
+        }}/>
+
+      {/* LAYER 5 */}  
+      <div 
+        className={styles.layer}
+        style={{
+          backgroundImage: `url(${layer_5})`,
+          left: 0,
+          right: 0,
+        }}/>
+
+      {/* LAYER 4 */}
+      <div
+        ref={layerFour.ref}
+        className={styles.layer} 
+        style={{
+          backgroundImage: `url(${layer_4})`,
+          left: layerFourExtend,
+          right: layerFourExtend,
+        }}/>
+      
+      {/* LAYER 3 */}
+      <div 
+        ref={layerThree.ref} 
+        className={styles.layer} 
+        style={{
+          backgroundImage: `url(${layer_3})`, 
+          left: layerThreeExtend,
+          right: layerThreeExtend,
+        }}/>
+
+      {/* LAYER 2 */}
+      <div 
+        ref={layerTwo.ref} 
+        className={styles.layer} 
+        style={{
+          backgroundImage: `url(${layer_2})`,
+          left: layerTwoExtend,
+          right: layerTwoExtend,
+        }}/>
+      
+      {/* LAYER 1 */}
+      <div 
+        ref={layerOne.ref} 
+        className={styles.layer} 
+        style={{
+          backgroundImage: `url(${layer_1})`, 
+          left: layerOneExtend, 
+          right: layerOneExtend
+        }}/>
+
     </div>
   )
+
 }
